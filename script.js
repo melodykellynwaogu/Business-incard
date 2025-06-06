3391538539ebd1e033bb8cc5577fb28e43f04da8
Melody Kelly. N <melodynwaogu224@gmail.com>
2025-06-06 10:56:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-window.addEventListener('DOMContentLoaded', () => 
-{
+window.addEventListener('DOMContentLoaded', () => {
     const card = document.getElementById('card');
     const frontCard = document.querySelector('.flip-card-front');
-    const backCard = document.querySelector('flip-card-back');
+    const backCard = document.querySelector('.flip-card-back');
 
+    
     setTimeout(() => {
         frontCard.classList.add('show-card');
     }, 500);
 
-    setTimeout(() => {
-        backCard.classList.add('show-card');
-    }, 1200);
-
-
+   
     card.addEventListener('click', () => {
         card.classList.toggle('flipped');
+        
+        if (card.classList.contains('flipped')) {
+            setTimeout(() => {
+                backCard.classList.add('show-card');
+            }, 300);
+        } else {
+            backCard.classList.remove('show-card');
+        }
     });
 });
